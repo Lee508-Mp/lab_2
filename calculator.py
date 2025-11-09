7e2588b4e0743a5272fd57c73aaeab214e40f988
Lee508-Mp <goldenmwangala08@gmail>
2025-11-09 14:58:23 +0200

    First commit

---
+++ calculator.py
@@ -0,0 +1,26 @@
+# Global variables
+history = []
+operations_count = 0
+memory = 0
+
+# Part 1: Implement Basic Arithmetic Functions
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    if num2 == 0:
+        print("Error: Division by zero")
+        return "Error: Division by zero"
+    return num1 / num2
+
+def modulus(num1, num2):
+    if num2 == 0:
+        print("Error: Modulus by zero")
+        return "Error: Modulus by zero"
+    return num1 % num2
